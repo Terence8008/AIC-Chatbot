a601d388c9f26279edd95b2a5dc52146414f272d
Terence8008 <Terencelim41@gmail.com>
2024-05-31 01:06:30 +0800

    Update
    Change language model and make the bot response better

--- AIC Chatbot main.py
+++ AIC Chatbot main.py
@@ -1,41 +1,39 @@
 # Chatbot Main file is here
-# packages needed:  SpeechRecognition, tensorflow,python-dotenv, langchain,
-# langchain-openai (partner package)
+# packages needed:  SpeechRecognition, python-dotenv, langchain
 # langchain-community
+# langchain_huggingface
 # Huggingface-hub
-# Pyaudio (no import but need)
+# Pyaudio (no import but needed)
 # keyboard pacakge maybe keyboard-mac for mac
-# pyttsx3 (text to speech)z
+# pyttsx3 (text to speech)
 
 import keyboard
 import speech_recognition as sr
-from langchain_community.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-# import os
-# from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint
+import os
+from dotenv import load_dotenv
 import pyttsx3
 
-# os.environ['HUGGINGFACEHUB_API_TOKEN'] = 'Hugging_face_token'
-# load_dotenv()
-
 engine = pyttsx3.init()
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+load_dotenv()
 
-hub_llm = HuggingFaceHub(
-        repo_id="google/flan-t5-large",
-        model_kwargs={'temperature': 0.8, 'repetition_penalty': 1.2, 'num_return_sequences': 1, 'min_length': 40}
-        
-        ### temperature (float, optional, defaults to 1.0) – The value used to module the next token probabilities / The temperature of the sampling operation. 1 means regular sampling, 0 means always take the highest score, 100.0 is getting closer to uniform probability.
-        ### min_length/max_length: Integer to define the minimum/maximum length in tokens of the output summary.
-
-    )
+# temperature (float, optional, defaults to 1.0) – The value used to module the next token probabilities
+# The temperature of the sampling operation. 1 means regular sampling, 0 means always take the highest score,
+# 100.0 is getting closer to uniform probability.
+# min_length/max_length: Integer to define the minimum/maximum length in tokens of the output summary.
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+llm = HuggingFaceEndpoint(repo_id=repo_id, temperature=0.7, )
 
 # Language models (LLMs) require prompts to function.
-# A prompt is a set of instructions or inputs to guide the model’s response. 
+# A prompt is a set of instructions or inputs to guide the model’s response.
 # Accepts a set of parameters from the user that can be used to generate a prompt for a language model.
 prompt = PromptTemplate(
     input_variables=["question"],
-    template="Answer this question in a happy manner: {question}"
+    template="Your name is John, only answer what i have ask. Do not repeat yourself. Do not use any Emoji. Answer "
+             "this question in a happy manner: {question}"
 )
 
 
@@ -43,7 +41,6 @@ class Chatbot:
     # Constructor
     def __init__(self, name):
         print("----- Starting up", name, "-----")
-        self.name = name
         self.audioText = ""
         self.bot_response = ""
 
@@ -65,7 +62,6 @@ class Chatbot:
             self.audioText = recognizer.recognize_google(audio, language="en-US")
             # In this case, Google Web Speech API is used (recognize_google())
             # Alternatives: Microsoft Bing Speech, Wit.ai, IBM Speech to Text
-            self.audioText = recognizer.recognize_google(audio, language="en-US",)
             print("You :", self.audioText)
         except Exception:
             pass
@@ -78,19 +74,16 @@ class Chatbot:
 
     def response(self, audiotext):
         if audiotext != "":
-            hub_chain = LLMChain(prompt=prompt, llm=hub_llm, verbose=True)
+            hub_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
             self.bot_response = hub_chain.run(audiotext)
             print(self.bot_response)
             return self.bot_response
-            hub_chain = LLMChain(prompt=prompt, llm=hub_llm, verbose=True) # Chain to run queries against LLMs.
-            ### verbose parameter enables detailed output
-            ### Benificial for understanding the abstraction that LangChain provides under the hood, while executing our query.
-            print(hub_chain.run(audiotext))
+            # verbose parameter enables detailed output
+            # Beneficial for understanding the abstraction that LangChain provides under the hood,
+            # while executing our query.
+
         else:
             print("No input found please try again")
-
-    def wake_up(self, audio_text):
-        return True if self.name in audio_text.lower() else False
 
 
 # Boot the AI
@@ -108,3 +101,4 @@ if __name__ == "__main__":
             pass
 
 print("----- Closing down chatbot -----")
+
